54f01fafbf411ab0688fe2624dbf77cf35edcae9
Evan Dunbar <evdunbar@protonmail.com>
2024-10-28 09:53:14 -0500

    First pass at problem 19

--- 19/Problem.hs
+++ 19/Problem.hs
@@ -1,4 +1,6 @@
-import qualified Data.Map.Strict as Map
+import Data.List
+import Data.Map.Strict qualified as Map
+
 {-
  - How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
  -
@@ -14,7 +16,14 @@ monthToDays month year
     | month `elem` [4, 6, 9, 11] = 30
     | month == 2 = if isLeapYear year then 29 else 28
 
+solution :: Int -> Int -> Int
+solution start end = sum count
+  where
+    years = [start .. end]
+    yearMonth = concatMap (\year -> [(year, month) | month <- [1 .. 12]]) years
+    isoDates = concatMap (\(year, month) -> [(year, month, day) | day <- [1 .. monthToDays month year]]) yearMonth
+    isoDateDayOfWeek = zip isoDates (cycle ["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday"])
+    count = map (\((_, _, date), dayOfWeek) -> fromEnum $ date == 1 && dayOfWeek == "Sunday") isoDateDayOfWeek
+
 main = do
-    print $ isLeapYear 1600
-    print $ isLeapYear 1700
-    print $ isLeapYear 2024
+    print $ solution 1901 2000
