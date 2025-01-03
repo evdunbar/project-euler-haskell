81f29180b4302fd6bd2225074c0d3f101d065fe3
E Dunbar <edunbar02@gmail.com>
2025-01-03 02:12:43 -0600

    First pass at problem 21

--- 21/Problem.hs
+++ 21/Problem.hs
@@ -1,17 +1,29 @@
+import Data.List
+
 isqrt :: Int -> Int
 isqrt = floor . sqrt . fromIntegral
 
+-- numbers < n which divide evenly into n
 properDivisors :: Int -> [Int]
-properDivisors n = 1 : concatMap (\m -> let (quotient, modulus) = divMod n m in if modulus == 0 then [m, quotient] else []) [2 .. isqrt n]
-
-amicableNumbers :: ([Int], [Int]) -> ([Int], [Int])
-amicableNumbers (xs, y : ys) = (xs, ys)
-    where 
-amicableNumbers _ = ([], [])
-
-solution n = n
+properDivisors n = nub divisors
   where
-    nums = [1 .. n]
+    divisors = 1 : concatMap (\m -> let (quotient, modulus) = divMod n m in if modulus == 0 then [m, quotient] else []) [2 .. isqrt n]
+
+isAmicable :: [Int] -> Int -> Bool
+isAmicable dns a = a == db && a /= b
+  where
+    b = dns !! (a - 1)
+    db = dns !! (b - 1)
+
+amicableNumbers :: Int -> [Int]
+amicableNumbers n = takeWhile (< n) $ filter (isAmicable dns) ns
+  where
+    ns = [1 .. n]
+    ms = [1 .. ]
+    dns = map (sum . properDivisors) ms
+
+solution n = sum $ amicableNumbers n
 
 main = do
-    print $ properDivisors 1
+    print $ amicableNumbers 300
+    print $ solution 10000
